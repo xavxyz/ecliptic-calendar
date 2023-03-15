7f643d0b37392cb3109c6903684f7a134c59ad2e
Xavier <xavier.cazalot@gmail.com>
2023-03-15 07:45:32 +0100

    (∩◕ヮ◕)⊃━ ✧.*･｡ﾟLet the magic happen ✨

--- src/events.ts
+++ src/events.ts
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 import { google, calendar_v3 } from "googleapis";
+import fs from "fs-extra";
+import moment, { Moment } from "moment";
 
-import { DateTimeEvent, PeriodEvent } from "./utils";
+import { DateTimeEvent, hop, noRush, PeriodEvent } from "./utils";
 
 dotenv.config();
 
@@ -12,6 +14,13 @@ const auth = new google.auth.GoogleAuth({
     "https://www.googleapis.com/auth/calendar.events",
   ],
 });
+
+const eventTypeToColorIdHashtable = {
+  seasons: "2", // #7ae7bf
+  magnitudes: "4", // #ff887c
+  suns: "5", // #fbd75b
+  moons: "9", // #5484ed
+} as const;
 
 const calendar = google.calendar({
   version: "v3",
@@ -60,3 +69,55 @@ export function transformPeriodEventToCalendarEvent(periodEvent: PeriodEvent) {
     },
   };
 }
+
+async function letTheMagicHappen(
+  eventType: keyof typeof eventTypeToColorIdHashtable,
+  after?: Moment,
+  before?: Moment
+) {
+  await noRush();
+
+  const colorId = eventTypeToColorIdHashtable[eventType];
+
+  const events = await fs.readJson(`data/${eventType}.json`);
+
+  let filteredEvents = events;
+  if (after || before) {
+    filteredEvents = events.filter(
+      (event: DateTimeEvent) =>
+        (after ? moment(event.dateTime).isAfter(after) : true) &&
+        (before ? moment(event.dateTime).isBefore(before) : true)
+    );
+  }
+
+  console.log({ total: events.length, filtered: filteredEvents.length });
+
+  for (const event of filteredEvents) {
+    const { summary, start, end } =
+      eventType === "seasons"
+        ? transformPeriodEventToCalendarEvent(event as PeriodEvent)
+        : transformDateTimeEventToCalendarEvent(event as DateTimeEvent);
+
+    const res = await insertEvent({
+      summary,
+      colorId,
+      start,
+      end,
+    });
+
+    console.log(res);
+
+    await hop();
+  }
+}
+
+// (async () => {
+//   for (const waow of ["seasons", "magnitudes", "moons", "suns"]) {
+//     await letTheMagicHappen("seasons");
+//     await letTheMagicHappen("magnitudes");
+//     await letTheMagicHappen("moons");
+//     await letTheMagicHappen("suns");
+//   }
+
+//   console.log("Et voilà ! 😊");
+// })();
